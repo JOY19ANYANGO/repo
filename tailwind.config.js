b8214ecfa23c2f7eeceb05625007c610c97553cf
JOY19ANYANGO <joyanyango402@gmail.com>
2024-08-09 17:38:32 +0300

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src /**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        georgia: ['Georgia', 'serif'],
+        'dancing-script': ['Dancing Script', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
-
